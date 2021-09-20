277f29ee237bcf4fcba95a980c411ca9e90a38b4
dgprofile <90986919+dgprofile@users.noreply.github.com>
2021-09-20 13:06:36 -0400

    remodified

--- karma.conf.js
+++ karma.conf.js
@@ -3,15 +3,6 @@
 
 module.exports = function (config) {
   config.set({
-    browsers: ['Chrome', 'Chrome_without_security'], // You may use 'ChromeCanary', 'Chromium' or any other supported browser
-
-    // you can define custom flags
-    customLaunchers: {
-      Chrome_without_security: {
-        base: 'Chrome',
-        flags: ['--disable-web-security', '--disable-site-isolation-trials']
-      }
-    },
     basePath: "",
     frameworks: ["jasmine", "@angular-devkit/build-angular"],
     plugins: [
